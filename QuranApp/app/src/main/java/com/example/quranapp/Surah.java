041901bc16c84ac3d5bbf268f2e9c4a7e60fd293
Muzamil Ahmad <66633369+Muzamil-07@users.noreply.github.com>
2022-09-01 15:51:06 +0500

    Quran app

--- QuranApp/app/src/main/java/com/example/quranapp/Surah.java
+++ QuranApp/app/src/main/java/com/example/quranapp/Surah.java
@@ -5,12 +5,17 @@ import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class Surah extends AppCompatActivity {
     public DrawerLayout drawerLayout;
     public ActionBarDrawerToggle actionBarDrawerToggle;
+    NavigationView navView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +30,19 @@ public class Surah extends AppCompatActivity {
 
         // to make the Navigation drawer icon always appear on the action bar
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        navView=findViewById(R.id.navView);
+
+        navView.setNavigationItemSelectedListener((menuItem)->{
+
+            switch (menuItem.getItemId()){
+                case R.id.nav_parah:
+                    Intent i = new Intent(Surah.this,MainActivity.class);
+                    startActivity(i);
+                    break;
+            }
+
+            return true;
+        });
     }
 
 
